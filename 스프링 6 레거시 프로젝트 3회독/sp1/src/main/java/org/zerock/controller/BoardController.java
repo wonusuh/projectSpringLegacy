1664fcb2a2da30ea670bb55d058d5419ad40f166
서원우 <wonwooseo23@gmail.com>
2026-07-04 08:54:19 +0900

    p. 200

--- 스프링 6 레거시 프로젝트 3회독/sp1/src/main/java/org/zerock/controller/BoardController.java
+++ 스프링 6 레거시 프로젝트 3회독/sp1/src/main/java/org/zerock/controller/BoardController.java
@@ -1,6 +1,9 @@
 package org.zerock.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +14,36 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 @Log4j2
 public class BoardController {
+    @GetMapping("/list")
+    public void list() {
+	log.info("---------------------------------------------------------");
+	log.info("board list");
+    }
 
+    @GetMapping("/register")
+    public void register() {
+	log.info("---------------------------------------------------------");
+	log.info("board register");
+    }
+
+    @PostMapping("/register")
+    public String registerPost() {
+	log.info("---------------------------------------------------------");
+	log.info("board register post");
+	return "redirect:/board/list";
+    }
+
+    @GetMapping("/read/{bno}")
+    public String read(@PathVariable("bno") Long bno) {
+	log.info("---------------------------------------------------------");
+	log.info("board read");
+	return "/board/read";
+    }
+
+    @GetMapping("/modify/{bno}")
+    public String modifyGET(@PathVariable("bno") Long bno) {
+	log.info("---------------------------------------------------------");
+	log.info("board modify");
+	return "/board/modify";
+    }
 }
