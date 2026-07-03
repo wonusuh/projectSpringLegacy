d9ca7a5eb12eeba4b3d55b571156153086b92f03
서원우 <wonwooseo23@gmail.com>
2026-07-04 07:48:21 +0900

    중간커밋 p. 191

--- 스프링 6 레거시 프로젝트 3회독/sp1/src/main/java/org/zerock/controller/BoardController.java
+++ 스프링 6 레거시 프로젝트 3회독/sp1/src/main/java/org/zerock/controller/BoardController.java
@@ -1,5 +1,15 @@
 package org.zerock.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+@Log4j2
 public class BoardController {
 
 }
