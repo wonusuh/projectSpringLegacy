db150a50cdc6e7dde2fcf650844e8c8f5025b0a2
서원우 <wonwooseo23@gmail.com>
2026-07-03 15:27:53 +0900

    중간커밋 p. 189

---
+++ 스프링 6 레거시 프로젝트 3회독/sp1/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,77 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.dto.BoardDTO;
+
+import lombok.extern.log4j.Log4j2;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j2
+public class BoardMapperTests {
+    @Autowired
+    private BoardMapper boardMapper;
+
+    @Test
+    public void testInsert() {
+	BoardDTO boardDTO = BoardDTO.builder().title("title").content("content").writer("user00").build();
+
+	int insertCount = boardMapper.insert(boardDTO);
+	log.info("----------");
+	log.info("insertCount : " + insertCount);
+    }
+
+    @Test
+    public void testInsert2() {
+	BoardDTO boardDTO = BoardDTO.builder().title("title").content("content").writer("user00").build();
+
+	int insertCount = boardMapper.insert(boardDTO);
+	log.info("----------");
+	log.info("insertCount : " + insertCount);
+	log.info("==========");
+	log.info("BNO : " + boardDTO.getBno());
+    }
+
+    @Test
+    public void testSelectOne() {
+	Long bno = 6646L;
+	BoardDTO boardDTO = boardMapper.selectOne(bno);
+	log.info("board : " + boardDTO);
+    }
+
+    @Test
+    public void testRemove() {
+	Long bno = 6646L;
+	int removeCount = boardMapper.remove(bno);
+	log.info("------------------------------------------");
+	log.info("removeCount : " + removeCount);
+    }
+
+    @Test
+    public void testUpdate() {
+	BoardDTO boardDTO = BoardDTO.builder().bno(6646L).title("Update Title").content("Update Content").delFlag(false)
+		.build();
+
+	int updateCount = boardMapper.update(boardDTO);
+
+	log.info("--------------------------------");
+	log.info("updateCount : " + updateCount);
+    }
+
+    @Test
+    public void testList() {
+	List<BoardDTO> dtoList = boardMapper.list();
+	log.info("dtoList");
+	log.info(dtoList);
+
+	dtoList.stream().forEach((eachDto) -> {
+	    log.info(eachDto);
+	});
+    }
+}
