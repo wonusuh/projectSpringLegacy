9911a9ddf942319ac84eb2009f25bb4d0c8993a4
서원우 <wonwooseo23@gmail.com>
2026-07-28 14:00:55 +0900

    중간커밋

---
+++ 스프링 6 레거시 프로젝트/스프링 6 레거시 프로젝트 5회독/sp1/src/main/java/org/zerock/service/HelloService.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.zerock.service;
+
+/**
+ * 
+ */
+public class HelloService {
+
+	/**
+	 * 
+	 */
+	public HelloService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
